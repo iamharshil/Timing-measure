6307f02aec7b0093330c5b3c45a05fc6965ce4fe
Harshil Chudasama <74813791+iamharshil@users.noreply.github.com>
2020-12-13 14:52:33 +0530

    Add files via upload

---
+++ timing masure.py
@@ -0,0 +1,6 @@
+import time
+startTime = time.time()
+''' enter youre code here'''
+endTime = time.time()
+totaltime = endTime - startTime
+print("total time required to execute code is=",totaltime)
